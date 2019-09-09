df73889759381091e972d75d7faa9f34d7f1a6a4
Florian Reck <flo-reck@floatblog.de>
2019-09-09 11:24:35 +0000

    Samba has it's own interface now

--- docker-builds/create-network.sh
+++ docker-builds/create-network.sh
@@ -3,5 +3,5 @@ docker network create -d macvlan     \
   --subnet=172.17.17.0/24            \
   --gateway=172.17.17.1              \
   --ip-range=172.17.17.252/30        \
-  --aux-address='host=172.17.17.253' \
+  --aux-address='host=172.17.17.254' \
   -o parent=eth0 pubnet
--- docker-builds/start-network.sh
+++ docker-builds/start-network.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 ip link add pubnet-shim link eth0 type macvlan  mode bridge
-ip addr add 172.17.17.253/32 dev pubnet-shim
+ip addr add 172.17.17.254/32 dev pubnet-shim
 ip link set pubnet-shim up
 ip route add 172.17.17.252/30 dev pubnet-shim
+ip route add 172.17.17.253/30 dev pubnet-shim
